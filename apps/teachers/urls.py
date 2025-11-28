c131a0d693d341bd80a73fe4d4583c3e8e213331
Milan721166 <milansahoo7211662005@gmail.com>
2025-11-28 17:52:15 +0530

    add full course

--- apps/teachers/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('dashboard/', views.teacher_dashboard, name='teacher_dashboard'),
-    path('admin/management/', views.admin_teacher_management, name='admin_teacher_management'),
-]
